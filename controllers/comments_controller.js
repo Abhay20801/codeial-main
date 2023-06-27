2036a09295224a14679539bd881fd720593ed12a
Abhay20801 <102847697+Abhay20801@users.noreply.github.com>
2023-06-27 09:44:39 +0530

    deleting a comment and post

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,7 @@
 const Comment = require('../models/comment');
 const mongoose = require('mongoose');
 const Post = require('../models/post');
+const User = require('../models/user')
 
 module.exports.create = async function(req, res) {
     try {
@@ -20,3 +21,19 @@ module.exports.create = async function(req, res) {
         console.log('Error creating comment', err);
     }
 };
+
+
+module.exports.destroy = async function(req,res){
+    const comment = await Comment.findById(req.params.id);
+    const post = await Post.findById(comment.post);
+    if (comment.user == req.user.id || post.user == req.user.id){
+        let postId = comment.post;
+        await comment.deleteOne();
+
+       const post = await Post.findByIdAndUpdate(postId,{
+        $pull:{comments:req.params.id}});
+        return res.redirect('back');
+    } else{
+        return res.redirect('back');
+    }
+}
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = async function(req,res){
     await Post.create({
@@ -11,5 +12,23 @@ module.exports.create = async function(req,res){
         console.log(`Error in creating post ${err}`); 
         return;
     })
+}
+
+module.exports.destroy = async function(req,res){
+    const post = await Post.findById(req.params.id);
+    // .id means converting the object id into string
+    if(post.user == req.user.id){
+        await post.deleteOne();
+
+       await Comment.deleteMany({post:req.params.id}).then(()=>{
+            
+            return res.redirect('back');
+        }).catch((err)=>{
+            console.log(`Error deleting comments for a deleted post ${err}`);
+        })
+
+    } else{
+        return res.redirect('back');
+    }
 }
 
