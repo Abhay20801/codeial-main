c06b68196a4940e5cd063639042cd0d97cad9978
Abhay20801 <102847697+Abhay20801@users.noreply.github.com>
2023-07-22 00:58:15 +0530

    env.js

--- config/environment.js
+++ config/environment.js
@@ -44,7 +44,7 @@ const accessLogStream = rfs.createStream('access.log', {
  const production = {
     name: 'production',
     // assest_path : process.env.CODEIAL_ASSET_PATH,
-    asset_path : '/' + process.env.CODEIAL_ASSET_PATH,
+    asset_path :process.env.CODEIAL_ASSET_PATH,
     session_cookie_key : process.env.CODEIAL_SESSION_COOKIE_KEY,
     db: process.env.CODEIAL_DB,
     smtp: {
